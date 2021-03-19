e8a42ff8c03358cc39a760b9e9a398e7c2bf2fa3
Nayana Naik <75340668+nn-msft@users.noreply.github.com>
2021-03-19 12:18:08 -0400

    Update Startup.cs
    Hello World Agaain! to Hello World test-05-comm

--- Startup.cs
+++ Startup.cs
@@ -37,7 +37,7 @@ namespace NewRelicDemo_NN_01
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World Agaain!");
+                    await context.Response.WriteAsync("Hello World test-05-comm!");
                 });
                 endpoints.MapControllerRoute(
 
